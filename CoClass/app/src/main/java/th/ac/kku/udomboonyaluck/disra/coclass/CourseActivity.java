cb3c9bc5af86fb17570d450402380b3d05ce34a9
Disra <Disra_u@kkumail.com>
2019-05-18 16:20:44 +0700

    18/05/2019

--- CoClass/app/src/main/java/th/ac/kku/udomboonyaluck/disra/coclass/CourseActivity.java
+++ CoClass/app/src/main/java/th/ac/kku/udomboonyaluck/disra/coclass/CourseActivity.java
@@ -1,13 +1,32 @@
 package th.ac.kku.udomboonyaluck.disra.coclass;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CourseActivity extends AppCompatActivity {
 
     TextView course_act_name,course_act_teacher;
+    private List<Student> lstStudent;
+    private RecyclerView student_list;
+    private String code;
+    FirebaseDatabase database;
+    DatabaseReference dbRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +36,36 @@ public class CourseActivity extends AppCompatActivity {
         Intent intent = getIntent();
         course_act_name = findViewById(R.id.course_act_name);
         course_act_teacher = findViewById(R.id.course_act_teacher);
+        student_list = findViewById(R.id.student_list);
 
         course_act_name.setText(intent.getStringExtra("courseName"));
         course_act_teacher.setText(intent.getStringExtra("teacher"));
+        code = intent.getStringExtra("code");
 
+        database = FirebaseDatabase.getInstance();
+
+        lstStudent = new ArrayList<>();
+        dbRef = database.getReference("/courses/");
+        dbRef.child(code).child("students").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                lstStudent.clear();
+                for(DataSnapshot snapshot:dataSnapshot.getChildren()){
+                    Student student = snapshot.getValue(Student.class);
+                    assert student != null;
+                    lstStudent.add(student);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        assert lstStudent != null;
+        StudentRecyclerAdapter recyclerViewAdapter = new StudentRecyclerAdapter(this,lstStudent,code);
+        student_list.setLayoutManager(new LinearLayoutManager(this));
+        student_list.setAdapter(recyclerViewAdapter);
     }
 }
